1b6e02a2b8021b9ad212415af4d31f97a784adaa
Karan Patokar <karan.123patokar@gmail.com>
2023-07-20 00:15:37 +0530

    Removed back end code

--- server/questions/generator.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-api_ret = callApi()
-if [ condition 1 ]: 
-    echo "What is the " + api_ret["stadium"] + "of " + api_ret["club"] + "?"
-elif [ condition 2 ]:
-    echo "How many " + api_ret["stat"] + "has " + api_ret["player_name"] + "scored"?
-    etc.
